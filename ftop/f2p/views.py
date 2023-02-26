d3d231433aa9a3a7843af91eb04c1675f14559d5
Pranav-ONLY <shendkarpranv0@gmail.com>
2023-02-26 21:35:25 +0530

    just remaining events

--- ftop/f2p/views.py
+++ ftop/f2p/views.py
@@ -48,15 +48,60 @@ def card(request):
             Pgname = request.POST.get("lg-name").title()
             Pgn = request.POST.get("lg-contact-number").strip()
             Pgl= request.POST.get("lg-location").strip()
-            Ss = request.POST.get("ssc-school")
-            Sy = request.POST.get("ssc-passing-year")
-            Sp = request.POST.get("ssc-percentage")
-            Hs = request.POST.get("hsc-school")
-            Hy = request.POST.get("hsc-passing-year")
-            Hp = request.POST.get("hsc-percentage")
-            Ds = request.POST.get("diploma-school")
-            Dy = request.POST.get("diploma-passing-year")
-            Dp = request.POST.get("diploma-percentage")
+            Ss = request.POST.get("ssc-school").strip()
+            Sy = request.POST.get("ssc-passing-year").strip()
+            Sp = request.POST.get("ssc-percentage").strip()
+            Hs = request.POST.get("hsc-school").strip()
+            Hy = request.POST.get("hsc-passing-year").strip()
+            Hp = request.POST.get("hsc-percentage").strip()
+            Ds = request.POST.get("diploma-school").strip()
+            Dy = request.POST.get("diploma-passing-year").strip()
+            Dp = request.POST.get("diploma-percentage").strip()
+
+            fsm1 = request.POST.get("fy-sem1-total-marks")
+            fsp1 = request.POST.get("fy-sem1-percentage")
+            fsb1 = request.POST.get("fy-sem1-backlog")
+            fsr1 = request.POST.get("fy-sem1-remark")
+            fsm2 = request.POST.get("fy-sem2-total-marks")
+            fsp2 = request.POST.get("fy-sem2-percentage")
+            fsb2 = request.POST.get("fy-sem2-backlog")
+            fsr2 = request.POST.get("fy-sem2-remark")
+            fap = request.POST.get("fy-avg-percentage")
+            fr = request.POST.get("fy-remark")
+            
+
+            ssm1 = request.POST.get("sy-sem1-total-marks")
+            ssp1 = request.POST.get("sy-sem1-percentage")
+            ssb1 = request.POST.get("sy-sem1-backlog")
+            ssr1 = request.POST.get("sy-sem1-remark")
+            ssm2 = request.POST.get("sy-sem2-total-marks")
+            ssp2 = request.POST.get("sy-sem2-percentage")
+            ssb2 = request.POST.get("sy-sem2-backlog")
+            ssr2 = request.POST.get("sy-sem2-remark")
+            sap = request.POST.get("sy-avg-percentage")
+            sr = request.POST.get("sy-remark")
+
+            tsm1 = request.POST.get("ty-sem1-total-marks")
+            tsp1 = request.POST.get("ty-sem1-percentage")
+            tsb1 = request.POST.get("ty-sem1-backlog")
+            tsr1 = request.POST.get("ty-sem1-remark")
+            tsm2 = request.POST.get("ty-sem2-total-marks")
+            tsp2 = request.POST.get("ty-sem2-percentage")
+            tsb2 = request.POST.get("ty-sem2-backlog")
+            tsr2 = request.POST.get("ty-sem2-remark")
+            tap = request.POST.get("ty-avg-percentage")
+            tr = request.POST.get("ty-remark")
+
+            lsm1 = request.POST.get("ly-sem1-total-marks")
+            lsp1 = request.POST.get("ly-sem1-percentage")
+            lsb1 = request.POST.get("ly-sem1-backlog")
+            lsr1 = request.POST.get("ly-sem1-remark")
+            lsm2 = request.POST.get("ly-sem2-total-marks")
+            lsp2 = request.POST.get("ly-sem2-percentage")
+            lsb2 = request.POST.get("ly-sem2-backlog")
+            lsr2 = request.POST.get("ly-sem2-remark")
+            lap = request.POST.get("ly-avg-percentage")
+            lr = request.POST.get("ly-remark")
             
          
 
@@ -74,5 +119,14 @@ def card(request):
         "Bf":Bf, "Hl":Hl, "Pfname":Pfname, "Pfo":Pfo, "Pfn":Pfn, "Pfl":Pfl, "Pfon":Pfon,
         "Pmname":Pmname, "Pmo":Pmo, "Pmn":Pmn, "Pml":Pml, "Pmon":Pmon, "Pgname":Pgname,
         "Pgn":Pgn, "Pgl":Pgl, "Ss":Ss, "Sy":Sy, "Sp":Sp, "Hs":Hs, "Hy":Hy, "Hp":Hp,
-        "Ds":Ds, "Dy":Dy, "Dp":Dp
+        "Ds":Ds, "Dy":Dy, "Dp":Dp, "fsm1":fsm1, "fsp1":fsp1, "fsb1":fsb1, "fsr1":fsr1,
+        "fsm2":fsm2, "fsp2":fsp2, "fsb2":fsb2, "fsr2":fsr2, 
+        "ssm1":ssm1,"ssp1":ssp1, "ssb1":ssb1, "ssr1":ssr1, "ssm2":ssm2,
+        "ssp2":ssp2, "ssb2":ssb2, "ssr2":ssr2, "tsm1":tsm1,"tsp1":tsp1,
+        "tsb1":tsb1, "tsr1":tsr1, "tsm2":tsm2, "tsp2":tsp2, "tsb2":tsb2,
+        "tsr2":tsr2, "lsm1":lsm1,"lsp1":lsp1,
+        "lsb1":lsb1, "lsr1":lsr1, "lsm2":lsm2, "lsp2":lsp2, "lsb2":lsb2,
+        "lsr2":lsr2, "fap":fap, "fr":fr, "sap":sap, "sr":sr,
+        "tap":tap, "tr":tr, "lap":lap, "lr":lr
+
     })
